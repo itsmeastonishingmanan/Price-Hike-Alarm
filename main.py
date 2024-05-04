7ef2e90d573d1862ac4c07e62fee16afbe36b76b
itsmeastonishingmanan <121456565+itsmeastonishingmanan@users.noreply.github.com>
2024-05-04 17:00:54 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,42 @@
+import requests
+from bs4 import BeautifulSoup
+import lxml
+import pprint
+import smtplib
+
+
+
+URL="https://www.amazon.com/dp/B075CYMYK6?psc=1&ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6"
+
+header={
+    "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+    "Accept-Language":"en-US,en;q=0.9",
+}
+
+
+response=requests.get(URL,headers=header)
+amazon_website=response.text
+#print(amazon_website)
+soup=BeautifulSoup(amazon_website,"lxml")
+print(soup.prettify())
+
+price=soup.find(name="span",class_="a-price-whole").getText()
+price_as_float = float(price)
+print(price_as_float)
+
+title = soup.find(id="productTitle").get_text().strip()
+print(title)
+
+BUY_PRICE = 200
+
+if price_as_float < BUY_PRICE:
+    message = f"{title} is now {price}"
+
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        result = connection.login(YOUR_EMAIL, YOUR_PASSWORD)
+        connection.sendmail(
+            from_addr=YOUR_EMAIL,
+            to_addrs=YOUR_EMAIL,
+            msg=f"Subject:Amazon Price Alert!\n\n{message}\n{url}".encode("utf-8")
+        )
